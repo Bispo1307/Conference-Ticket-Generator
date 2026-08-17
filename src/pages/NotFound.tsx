397dae7f6de3754090d531e040f7bdd4b752fb12
Alan <alanoliveirab09@gmail.com>
2026-08-17 16:58:51 -0300

    fix: text gradient color

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -22,7 +22,15 @@ export const NotFound = () => {
           <img className="w-54 pb-12 mx-auto" src={logoFull} alt="Logo" />
         </div>
         <h1 className="mt-6 font-extrabold text-9xl text-center text-neutral-0">
-          <span className="bg-linear-to-r from-orange-500 to-white bg-clip-text text-transparent">
+          {/* <span className="bg-linear-to-r from-orange-500 to-white bg-clip-text text-transparent">
+            404
+          </span> */}
+          <span
+            className="text-transparent bg-clip-text"
+            style={{
+              backgroundImage: "linear-gradient(to right, #f97316, #ffffff)",
+            }}
+          >
             404
           </span>
         </h1>
--- src/pages/Ticket.tsx
+++ src/pages/Ticket.tsx
@@ -40,8 +40,16 @@ export const Ticket = () => {
         <div>
           <h1 className="mx-0 md:mx-4 lg:mx-0 font-extrabold text-3xl md:text-5xl lg:text-6xl text-center text-neutral-0">
             Parabéns,{" "}
-            <span className="bg-linear-to-r from-orange-500 to-white bg-clip-text text-transparent">
+            {/* <span className="bg-linear-to-r from-orange-500 to-white bg-clip-text text-transparent">
               {data.fullName ? data.fullName : "Lucas Silva"}
+            </span> */}
+            <span
+              className="bg-clip-text text-transparent"
+              style={{
+                backgroundImage: "linear-gradient(to right, #f97316, #ffffff)",
+              }}
+            >
+              {data.fullName || "Lucas Silva"}
             </span>
             ! Seu ingresso está pronto.
           </h1>
